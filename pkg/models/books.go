5eac229bd1d38b76f8dd02d705b0600b59287186
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-25 10:28:12 +0300

    Model fields for books and authors are specialized in terms of psql

--- pkg/models/books.go
+++ pkg/models/books.go
@@ -1,17 +1,17 @@
 package models
 
 type Book struct {
-	ID          string
-	Name        string
+	ID          string `gorm:"primaryKey;unique"`
+	Name        string `gorm:"size:50;not null"`
 	PageNumber  int
-	StockNumber int
-	Price       float64
-	StockCode   string
-	ISBN        string
+	StockNumber int     `gorm:"type:INT;not null"`
+	Price       float64 `gorm:"type:FLOAT8;not null"`
+	StockCode   string  `gorm:"size:6;not null"`
+	ISBN        string  `gorm:"size:10;not null"`
 	Author      Author
 }
 
 type Author struct {
-	Name string
-	ID   string
+	Name string `gorm:"size:50;not null"`
+	ID   string `gorm:"size:4;not null"`
 }

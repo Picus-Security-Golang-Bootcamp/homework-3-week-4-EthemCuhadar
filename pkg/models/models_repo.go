f5d924e074fb9b80a543a5b7822579cb57591ce3
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-25 12:27:23 +0300

    Sample data can be inserted to database now

--- pkg/models/models_repo.go
+++ pkg/models/models_repo.go
@@ -10,7 +10,15 @@ func NewBookRepository(db *gorm.DB) *BookRepository {
 	return &BookRepository{db: db}
 }
 
-func (b *BookRepository) Migrations() {
-	b.db.AutoMigrate(&Book{})
-	b.db.AutoMigrate(&Author{})
+func (br *BookRepository) Migrations() {
+	br.db.AutoMigrate(&Book{})
+	br.db.AutoMigrate(&Author{})
+}
+
+func (br *BookRepository) InsertSampleData(list []Book) {
+	for _, book := range list {
+		br.db.Where(Book{ID: book.ID}).
+			Attrs(Book{ID: book.ID, Name: book.Name, PageNumber: book.PageNumber, StockNumber: book.StockNumber, Price: book.Price, StockCode: book.StockCode, ISBN: book.ISBN, Author: book.Author}).
+			FirstOrCreate(&book)
+	}
 }

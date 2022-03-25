ff1f2801c39fb6e396af445cf58788f06cc6f224
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-25 12:19:08 +0300

    User can create new book repository and migrate function declared

---
+++ pkg/models/models_repo.go
@@ -0,0 +1,16 @@
+package models
+
+import "gorm.io/gorm"
+
+type BookRepository struct {
+	db *gorm.DB
+}
+
+func NewBookRepository(db *gorm.DB) *BookRepository {
+	return &BookRepository{db: db}
+}
+
+func (b *BookRepository) Migrations() {
+	b.db.AutoMigrate(&Book{})
+	b.db.AutoMigrate(&Author{})
+}
